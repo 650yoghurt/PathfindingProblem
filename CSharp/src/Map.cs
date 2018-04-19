32fc0ec7ad4bbf68c4d1517ba69a276ef5a2cf14
yoghurt <tobj@itu.dk>
2018-04-19 20:07:58 +0200

    works

--- CSharp/src/Map.cs
+++ CSharp/src/Map.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Collections;
 
 namespace PathfindingCSharp
 {
@@ -101,9 +102,63 @@ namespace PathfindingCSharp
             return sb.ToString();
         }
 
-        public void ComputePath()
-        {           
-            //TODO: Implement solution here
+        private Queue queue;
+
+        public bool ComputePath()
+        { 
+            var edgeTo = new (int, int)[32,32];
+            var visited = new bool[32, 32];
+            queue = new Queue();
+
+            queue.Enqueue(new Node { x = startPosition.x, y = startPosition.y });
+            visited[startPosition.y, startPosition.x] = true;
+
+            var res = search(edgeTo, visited);
+
+            if (res) drawPath(edgeTo, goalPosition.x, goalPosition.y);;
+            Console.WriteLine("Returned: " + res);
+
+            return res;
+        }
+
+        public void drawPath((int, int)[,] edgeTo, int x, int y){
+            if ((x == startPosition.x && y == startPosition.y)) return;
+            mapPositions[y, x] = pathChar;
+            var k = edgeTo[y, x];
+            drawPath(edgeTo, k.Item1, k.Item2, --i);
+        }
+
+        public bool insideBounds(int i, int j){
+            return i >= 0 && j >= 0 && i < 32 && j < 32;
+        }
+
+        public bool search((int, int)[,] edgeTo, bool[,] visited)
+        {
+            if (queue.Count == 0) return false;
+            Node n = (Node)queue.Dequeue();
+
+            for (int i = -1; i <= 1; i++){
+                for (int j = -1; j <= 1; j++){
+                    int x = n.x + i;
+                    int y = n.y + j;
+                    if (x == goalPosition.x && y == goalPosition.y) {
+                        Console.WriteLine("Found it!");
+                        edgeTo[y, x] = (n.x, n.y);
+                        return true;
+                    }
+                    if (insideBounds(x,y) && mapPositions[y,x] == emptyChar && (!visited[y,x])) {
+                        edgeTo[y, x] = (n.x, n.y);
+                        visited[y, x] = true;
+                        queue.Enqueue(new Node { x = x, y = y });
+                    }
+                }
+            }
+
+            return search(edgeTo, visited);
+        }
+        public class Node{
+            public int x;
+            public int y;
         }
     };
 }
